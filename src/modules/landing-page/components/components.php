55fa7bc9b44920ed5ac262cf77006d94963d9580
ksagun <81825913+ksagun@users.noreply.github.com>
2023-08-04 00:21:47 +0800

    Update components.php

--- src/modules/landing-page/components/components.php
+++ src/modules/landing-page/components/components.php
@@ -1,9 +1,13 @@
 <?php  
-   $files = glob(__DIR__.'/*', GLOB_ONLYDIR);
+   /*$files = glob(__DIR__.'/*', GLOB_ONLYDIR);
 
    foreach($files as $component){
       $name = explode("/",$component);
       include($component."/".$name[1].".component.php");
-   }
+   }*/
+
+  spl_autoload_register(function ($class_name) {
+      include $class_name.'/'.$class_name . '.component.php';
+  });
 
 ?>
