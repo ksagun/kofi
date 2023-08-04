8c33149f1d69f205f632df9f93e05314f1303889
ksagun <ksagun@abckidsny.com>
2023-08-05 07:40:28 +0800

    Updated component loading logic

--- src/modules/landing-page/components/components.php
+++ src/modules/landing-page/components/components.php
@@ -1,13 +1,12 @@
 <?php  
-   /*$files = glob(__DIR__.'/*', GLOB_ONLYDIR);
+  class Components {
+      public function initComponents(){
+         $files = glob(__DIR__.'/*', GLOB_ONLYDIR);
 
-   foreach($files as $component){
-      $name = explode("/",$component);
-      include($component."/".$name[1].".component.php");
-   }*/
-
-  spl_autoload_register(function ($class_name) {
-      include $class_name.'/'.$class_name . '.component.php';
-  });
-
+         foreach($files as $component){
+            $name = explode("/",$component);
+            include($component."/".$name[1].".component.php");
+         }
+      }
+  }
 ?>
--- src/modules/landing-page/index.php
+++ src/modules/landing-page/index.php
@@ -2,12 +2,13 @@
     include_once('landing-page.controller.php');
     include_once('landing-page.form.php');
     
-    // Call components php file to autoload components
-    include_once('components/components.php');
-
     class LandingPageModule extends LandingPageController {
 
         public function init(){
+            include_once('components/components.php');
+            $components = new Components();
+            $components->initComponents();
+            
             include($this->template_name);
             define("CONTROLLER_JS", $this->js);
             define("CONTROLLER_STYLE", $this->style);
